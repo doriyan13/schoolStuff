c4e48a1df76fb86abf4d9c61138e16f770cbcdc6
Doriyan Esterin <doriyan13@gmail.com>
2021-01-12 14:28:05 +0200

    adding the substract command to BigNumber class

--- src/com/company/BigNumber.java
+++ src/com/company/BigNumber.java
@@ -67,6 +67,8 @@ public class BigNumber {
      * @return
      */
     public String toString(){
+        //TODO: this method must be in O(n) -
+
         String result = "";
         IntNode curr_Node = _head;
 
@@ -78,6 +80,162 @@ public class BigNumber {
             curr_Node = curr_Node.getNext();
         }
         // Return the final String -
+        return result;
+    }
+
+    /**
+     *
+     * @param other
+     * @return
+     */
+    public int compareTo (BigNumber other){
+        String curr_Big_Number = this.toString();
+        String other_Big_Number = other.toString();
+        if(curr_Big_Number.length() > other_Big_Number.length()){
+            return 1;
+        }
+        if(curr_Big_Number.length() < other_Big_Number.length()){
+            return -1;
+        }
+        else {
+            //TODO: need to ask if i can use compareTo ofString? if not then i must count my self and test!!!
+            return curr_Big_Number.compareTo(other_Big_Number);
+        }
+    }
+
+    /**
+     * This method is getting a Big number and add it to my current BigNumber and return a new BigNumber which is the addition of both of them.
+     * @param other
+     * @return
+     */
+    public BigNumber addBigNumber (BigNumber other){
+        IntNode curr_Index = this._head;
+        IntNode other_Index = other._head;
+
+        int temp_Sum = 0, single_Num_Of_Sum = 0;
+        // First i will start by creating the new BigNumber that will have a defined _head.
+        BigNumber result = new BigNumber();
+        IntNode result_Index = result._head;
+
+        while (other_Index != null){
+            //TODO: see what happen when you try to add null with val?
+            temp_Sum += curr_Index.getValue() + other_Index.getValue();
+            single_Num_Of_Sum = temp_Sum % 10;
+            // Sending an integer to a method that expect Long, yet it's fine because in the compilation and run time java know how to handle it automaticly!
+            result_Index.setValue(single_Num_Of_Sum);
+            // Maintain the extra that left -
+            temp_Sum -= single_Num_Of_Sum;
+
+            if(other_Index.getNext() != null) {
+                // Create the next spot and going to point it -
+                result_Index.setNext(new IntNode(0));
+                result_Index = result_Index.getNext();
+            }
+            // Go to the next spot in each BigNumber -
+            if(curr_Index != null) {
+                curr_Index = curr_Index.getNext();
+            }
+            other_Index = other_Index.getNext();
+        }
+
+        if(temp_Sum > 0){
+            while (curr_Index != null){
+
+                temp_Sum += curr_Index.getValue();
+                single_Num_Of_Sum = temp_Sum % 10;
+                // Sending an integer to a method that expect Long, yet it's fine because in the compilation and run time java know how to handle it automaticly!
+                result_Index.setValue(single_Num_Of_Sum);
+                // Maintain the extra that left -
+                temp_Sum -= single_Num_Of_Sum;
+
+                if(curr_Index.getNext() != null) {
+                    // Create the next spot and going to point it -
+                    result_Index.setNext(new IntNode(0));
+                    result_Index = result_Index.getNext();
+                }
+            }
+
+            while (temp_Sum > 0){
+                single_Num_Of_Sum = temp_Sum % 10;
+                // Sending an integer to a method that expect Long, yet it's fine because in the compilation and run time java know how to handle it automaticly!
+                result_Index.setValue(single_Num_Of_Sum);
+                // Maintain the extra that left -
+                temp_Sum -= single_Num_Of_Sum;
+
+                if(temp_Sum > 0) {
+                    // Create the next spot and going to point it -
+                    result_Index.setNext(new IntNode(0));
+                    result_Index = result_Index.getNext();
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param other
+     * @return
+     */
+    public BigNumber subtractBigNumber(BigNumber other){
+        // Will hold my result -
+        BigNumber result = new BigNumber();
+
+        // If they are equal -
+        if(this.compareTo(other) == 0){
+            return result;
+        }
+        if(other == null){
+            return new BigNumber(this);
+        }
+        // If This BigNumber is bigger then other -
+        else if(this.compareTo(other) == 1){
+            return substractTwoBigNumbers(this,other,result);
+        }
+        // If this not equal to other and the other BigNumber is bigger then him, he must be smaller then him -
+        else{
+            return substractTwoBigNumbers(other,this,result);
+        }
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Private Methods of this class -
+    //TODO: Private Method that i need to add API!!!
+    private BigNumber substractTwoBigNumbers(BigNumber bigger_Big_Number , BigNumber other, BigNumber result){
+        IntNode curr_Index = this._head;
+        IntNode other_Index = other._head;
+
+        int temp_Sum = 0, single_Num_Of_Sum = 0;
+        IntNode result_Index = result._head;
+
+        while (curr_Index != null){
+            //TODO: see what happen when you try to add null with val?
+            temp_Sum = curr_Index.getValue() - other_Index.getValue();
+
+            if(temp_Sum < 0){
+                // moving the bigger number -1 :
+                result_Index.getNext().setValue(result_Index.getValue() - 1);
+                temp_Sum = 10 - temp_Sum;
+            }
+            //Setting the finale result of this figure -
+            result_Index.setValue(temp_Sum);
+            // Resetting the sum after filling him -
+            temp_Sum = 0;
+
+            if(other_Index.getNext() != null) {
+                // Create the next spot and going to point it -
+                result_Index.setNext(new IntNode(0));
+                result_Index = result_Index.getNext();
+            }
+            // Go to the next spot in each BigNumber -
+            if(other_Index != null) {
+                other_Index = other_Index.getNext();
+            }
+            else if(other_Index == null){
+                other_Index = new IntNode(0);
+            }
+            curr_Index = curr_Index.getNext();
+        }
         return result;
     }
 }
